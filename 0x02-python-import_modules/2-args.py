5eab57c5ef91efc10cf3ce7f3d921fea0998df41
Razig17 <razog1999@gmail.com>
2024-01-19 03:55:42 +0300

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if (argc == 0):
         print("0 arguments.")
     elif (argc == 1):
-        print(f"1 argument\n1: {argv[1]}")
+        print(f"1 argument:\n1: {argv[1]}")
     else:
         print(f"{argc} arguments:")
         for i in range(1, argc + 1):
