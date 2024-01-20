bf3ec615ac5382d8380f8b685211786bed3ef8c5
Razig17 <razog1999@gmail.com>
2024-01-20 10:14:46 +0300

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in matrix:
-            l = len(i)    
-            for j in range(l):
-                if j == l - 1:
-                    print("{:d}".format(i[j]))
-                else:
-                    print("{:d}".format(i[j]), end=" ")
+
+    for i in matrix:
+        len_i = len(i)
+        for j in range(len_i):
+            if j == len_i - 1:
+                print("{:d}".format(i[j]), end="")
+            else:
+                print("{:d}".format(i[j]), end=" ")
+        print()
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
