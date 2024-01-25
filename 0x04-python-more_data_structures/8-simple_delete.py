3ad199c3d986fd444599d12e84b6b922205e85ab
Razig17 <razog1999@gmail.com>
2024-01-25 04:21:25 +0300

    task 8 and 9

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,5 +2,5 @@
 
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        a_dictionary.remove(key)
+       del a_dictionary(key)
     return a_dictionary
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    {k: v * 2 for k, v in a_dictionary.items()}
+    
+    return {i: j * 2 for i, j in a_dictionary.items()}
