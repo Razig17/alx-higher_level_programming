1945ccec2402b12ad6cc73cafb0bab74316c284c
Razig17 <razog1999@gmail.com>
2024-01-25 05:51:45 +0300

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,10 +6,10 @@ def roman_to_int(roman_string):
     if type(roman_string) == str:
         pre = roman.get(roman_string[0])
         for c in roman_string:
-            
+
             if roman.get(c) <= pre:
                 num += roman.get(c)
-                
+
             else:
                 num = roman.get(c) - num
             pre = roman.get(c)
