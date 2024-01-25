38a319bce8240a40e0e260592bd69b39c9d3dddb
Razig17 <razog1999@gmail.com>
2024-01-25 06:38:29 +0300

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,16 @@
 
 def roman_to_int(roman_string):
     num = 0
+    subtract = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if type(roman_string) == str:
-        pre = roman.get(roman_string[0])
+        pre = roman_string[0]
         for c in roman_string:
 
-            if roman.get(c) <= pre:
-                num += roman.get(c)
-
+            if pre + c in subtract.keys():
+                print("found", pre + c)
+                num += subtract.get(pre + c) - roman.get(pre)
             else:
-                num = roman.get(c) - num
-            pre = roman.get(c)
+                num = roman.get(c) + num
+            pre = c
     return num
