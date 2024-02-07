2e4a24112d882bdc719692c419c9c9e4f3c3143e
Razig17 <razog1999@gmail.com>
2024-02-07 03:15:06 +0300

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""square class with a size"""
+
+
+class Square:
+    """"A class Square that defines a square by size"""
+
+    def __init__(self, size=0):
+        """Initialize the size with a postive integer"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,10 +4,10 @@
 
 
 class Square:
-    """class that defines a square with size"""
+    """"A class Square that defines a square by size"""
 
     def __init__(self, size=0):
-        """Initialize the size with a positive value"""
+        """Initialize the size with a postive integer"""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
