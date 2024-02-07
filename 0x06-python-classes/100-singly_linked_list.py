a885817c0cfcea8a52cc85de997b5c4d11ed56fe
Razig17 <razog1999@gmail.com>
2024-02-07 05:41:47 +0300

    task 8

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
-"""" Node class"""
+Square = __import__('101-square').Square
 
-class Node:
-        """defines a node of a singly linked list"""
-        
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
+
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,73 @@
+"""square class"""
+
+
+class Square:
+    """"A class Square that defines a square by size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the size and position"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieves the size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """"Sets the size value"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ Retriieves the position"""
+
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """Sets the position"""
+
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = value
+
+    def area(self):
+        """Calculates the area of the square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints the square"""
+        if self.__size == 0:
+            print()
+            return
+
+        for i in range(self.__position[1]):
+            print()
+
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        pos = ""
+        if self.__size == 0:
+            return "\n"
+        for w in range(self.__position[1]):
+            pos += "\n"
+        for w in range(self.__size):
+            for i in range(self.__position[0]):
+                pos += " "
+            for j in range(self.__size):
+                pos += "#"
+            pos += "\n"
+        return pos
