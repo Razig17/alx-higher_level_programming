fa629b9ce5085f70776b14d846369a223922630c
Razig17 <razog1999@gmail.com>
2024-02-07 04:33:03 +0300

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,8 +19,12 @@ class Square:
         """Prints the square"""
         if self.__size == 0:
             print()
+            return
+        for i in range(self.__position[1]):
+            print()
         for i in range(self.__size):
             for i in range(self.__size):
+                print("" * self.position[0])
                 print("#", end="")
             print()
 
