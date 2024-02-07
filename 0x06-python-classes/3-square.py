f15afa5ad9e29195c82a402f6966365300ba6c8c
Razig17 <razog1999@gmail.com>
2024-02-07 03:18:59 +0300

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,3 +15,8 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+
+        return (self.__size * self.__size)
