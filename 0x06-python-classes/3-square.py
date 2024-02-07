305c960e9e00a3fb738c4b6b6645339a5295d16e
Razig17 <razog1999@gmail.com>
2024-02-07 03:11:26 +0300

    task 2

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,9 +9,9 @@ class Square:
     def __init__(self, size=0):
         """Initialize the size with a postive integer"""
 
-        
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
