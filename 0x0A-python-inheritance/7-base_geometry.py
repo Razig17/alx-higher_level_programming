1bcdbf242e9f26090cb3b7468cc0ec09031f41f1
Razig17 <razog1999@gmail.com>
2024-02-20 03:25:48 +0300

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""BaseGeometry class """
+
+
+class BaseGeometry:
+    """"A class named BaseGeometry"""
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Check value if it is a postive integer"""
+        if (type(value) is not int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
