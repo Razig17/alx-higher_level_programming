5c66a45be5c725033941baf7282bd914f21ad048
Razig17 <razog1999@gmail.com>
2024-02-20 05:31:42 +0300

    Task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""A module contains "Square" class"""
+
+
+def add_attribute(obj, attribute_name, attribute_value):
+    """Adds a new attribute to an object if its possible"""
+
+    if not hasattr(obj, '__dict__') or hasattr(type(obj), '__slots__'):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, attribute_name, attribute_value)
+class MyClass():
+    pass
