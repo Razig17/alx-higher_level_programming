e22e5164331c8cf1db09dd091bec403e084b2cac
Razig17 <razog1999@gmail.com>
2024-02-21 05:38:14 +0300

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""This module contanis a class Student"""
+
+
+class Student():
+    """A Studnet class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize first_name, last_name and age"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs):
+        """Retrieves a dictionary representation of a Student instance"""
+        json = {"first_name": self.first_name, "last_name": self.last_name,
+                "age": self.age}
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return json
+            return {key: val for key, val in json.items() if key in attrs}
+
+        return json
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This module contanis a class Student"""
+
+
+class Student():
+    """A Studnet class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize first_name, last_name and age"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance"""
+        json = {"first_name": self.first_name, "last_name": self.last_name,
+                "age": self.age}
+        return json
