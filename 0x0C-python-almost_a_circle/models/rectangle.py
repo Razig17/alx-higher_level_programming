35a7e1bfc972744c379a1f4ce669b09aa2a27e4c
Razig17 <razog1999@gmail.com>
2024-02-26 20:15:43 +0300

    Rectangle Class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+
+
+"""This module contains "Rectangle" class"""
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """Rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize x y width, and height"""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Retrieves width"""
+        return(self.__width)
+
+    @width.setter
+    def width(self, width):
+        """Sets the width"""
+
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = width
+
+    @property
+    def height(self):
+        """Retrieves height value"""
+        return(self.__height)
+
+    @height.setter
+    def height(self, height):
+        """Sets the height"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = height
+
+    @property
+    def x(self):
+        """Retrieves x value"""
+        return(self.__x)
+
+    @x.setter
+    def x(self, x):
+        """Sets x"""
+
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = x
+
+    @property
+    def y(self):
+        """Retrieves x value"""
+        return(self.__y)
+
+    @y.setter
+    def y(self, y):
+        """Sets y"""
+
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    def area(self):
+        """Calculates the area of the rectangle"""
+
+        return (self.__width * self.__height)
+
+    def display(self):
+        """Prints the Rectangle with the character #"""
+        for i in range(self.y):
+            print()
+
+        for i in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " \
+            f"{self.__width}/{self.__height}"
+
+    def update(self, *args, **kwargs):
+        """Updates the attattributes """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'width':
+                    self.width = value
+                elif key == 'height':
+                    self.height = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+
+        return {
+                "id": self.id,
+                'width': self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+        }
