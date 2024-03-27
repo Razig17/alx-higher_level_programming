e2595df3d87e88e200ca5c7f664fd2703fbb9b67
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-27 05:47:18 +0300

    Task 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- SQL script to display the top 3 cities temperature during July and August ordered by temperature (descending)
+-- Select the city, calculate the average temperature in Fahrenheit, and order by temperature (descending)
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE MONTH(date) IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
