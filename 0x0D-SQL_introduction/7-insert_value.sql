658934ff385a913af410e227d05e21c6db9f690a
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-27 03:52:38 +0300

    Task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- SQL script to insert a new row into the table first_table in the database hbtn_0c_0
+-- Insert a new row into the table first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
