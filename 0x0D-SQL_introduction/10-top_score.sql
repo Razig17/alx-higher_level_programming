361857c098ad64be4f83870a150ac560422335b1
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-27 04:14:46 +0300

    Task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- SQL script to list all records of the table second_table from the database hbtn_0c_0
+-- Select all records from the table second_table, displaying both score and name, and order by score (top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
