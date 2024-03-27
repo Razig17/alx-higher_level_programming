0d34a4243ce00464ec00fc3e6f5bf0ace6e0ceb6
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-27 04:10:20 +0300

    Task 8

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- SQL script to display the number of records with id = 89 in the table first_table of the database hbtn_0c_0
+
+-- Count the number of records with id = 89 in the table first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
