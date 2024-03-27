760b8842595e59ec8c7bcfe864de3640ec98fb5c
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-27 04:30:29 +0300

    Task 13

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- SQL script that removes all records with a score <= 5 in the table second_table
+-- Delete all records from second_table where score is less than or equal to 5
+DELETE FROM second_table WHERE score <= 5;
