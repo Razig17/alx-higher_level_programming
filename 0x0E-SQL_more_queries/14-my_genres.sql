2e6634d27406cbe5617e94affd3e06903bc30e82
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-28 04:57:14 +0300

    Tasks 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,12 @@
--- SQL script to list all genres from hbtn_0d_tvshows and display the number of shows linked to each
+-- SQL script to list all genres of the show Dexter from the hbtn_0d_tvshows database
 
--- Select the genre name from the tv_genres table and count the number of linked shows for each genre
--- Group the results by genre id to ensure unique genres and count the number of shows linked to each genre
--- Filter out genres that don't have any shows linked (HAVING COUNT(show_id) > 0) and Order the results in descending order by the number of shows linked
+-- Select the genre name from the tv_genres table
+-- Join tv_genres, tv_show_genres, and tv_shows tables to establish the relationship between genres and shows
+-- Filter the results to include only genres related to the show Dexter
+-- Order the results in ascending order by the genre name
 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows  
-FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
-GROUP BY tv_genres.id HAVING COUNT(tv_show_genres.show_id) > 0
-ORDER BY number_of_shows DESC; 
+SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
