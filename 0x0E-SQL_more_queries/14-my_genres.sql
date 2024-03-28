798f2d5710fa1b4560bce982792878834f50add8
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-28 04:53:35 +0300

    Tasks 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- SQL script to list all genres from hbtn_0d_tvshows and display the number of shows linked to each
+
+-- Select the genre name from the tv_genres table and count the number of linked shows for each genre
+-- Group the results by genre id to ensure unique genres and count the number of shows linked to each genre
+-- Filter out genres that don't have any shows linked (HAVING COUNT(show_id) > 0) and Order the results in descending order by the number of shows linked
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows  
+FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+GROUP BY tv_genres.id HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC; 
