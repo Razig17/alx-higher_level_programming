9cee8eab6490ca0be059ac56d0207dffcc988490
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-28 03:38:20 +0300

    Tasks 5 to 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- SQL script to list all cities with corresponding state names from the database hbtn_0d_usa
 -- Select cities along with their corresponding state names using a JOIN operation
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
