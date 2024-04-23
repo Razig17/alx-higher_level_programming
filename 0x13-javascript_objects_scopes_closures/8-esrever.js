8e9041c91dcf405108c1b6e0ff7cc262de6247b1
Razig17 <abdelrazig.sharif1@gmail.com>
2024-04-24 00:19:10 +0300

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const arr = list.reverse();
+  const arr = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    arr.push(list[i]);
+  }
   return arr;
 };
