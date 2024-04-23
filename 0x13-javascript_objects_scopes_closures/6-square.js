b793581c971fd8730b65817158de38d19fd7a424
Razig17 <abdelrazig.sharif1@gmail.com>
2024-04-23 23:51:58 +0300

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
+      }
+    } else {
+      this.print();
+    }
+  }
+};
