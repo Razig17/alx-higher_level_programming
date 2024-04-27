f79096fef383ff6b00440173fe279693898b4a57
Razig17 <abdelrazig.sharif1@gmail.com>
2024-04-27 04:16:23 +0300

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 """
-A script that lists all states with a name starting with N
-from the database hbtn_0e_0_usa
+A script that lists all cities from the database
 """
 
 import sys
@@ -13,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.Connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM cities")
+    c.execute("SELECT cities.id cities.name states.name FROM cities JOIN states where cities.state_id = states.id")
     data = c.fetchall()
     for row in data:
         print(row)
