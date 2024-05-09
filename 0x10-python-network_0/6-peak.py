3bd74d76db1e104d3d57b432534612ee24a4f89b
Razig17 <abdelrazig.sharif1@gmail.com>
2024-05-10 00:35:27 +0300

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contain a function that finds a peak in a list of unsorted integers
+"""
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    if list_of_integers == None:
+        return None
+    else:
+       list_of_integers.sort()
+       return list_of_integers[-1]
